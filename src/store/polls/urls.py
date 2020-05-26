7d8dd9b95c94b7b5dd3636e8758d001f7b295f30
Kimmo Sääskilahti <kimmo.saaskilahti@silo.ai>
2020-05-26 15:42:07 +0300

    Add polls models.

--- src/store/polls/urls.py
+++ src/store/polls/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+]
--- src/store/store/settings.py
+++ src/store/store/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "polls.apps.PollsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
